62e8c586918399394523fdfccb661a25fd4a09e9
elias myers <eliasmyers@wifi-10-43-72-225.wifi.berkeley.edu>
2026-05-07 13:12:12 -0700

    byline: The Sale of San Francisco's Institutions (Berkeley Political Review)

--- bylines.js
+++ bylines.js
@@ -3,5 +3,6 @@
 // { title: "Article title", publication: "Publication Name", url: "https://...", date: "YYYY-MM-DD" }
 
 var BYLINES = [
+  { title: 'The Sale of San Francisco's Institutions', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2023/05/21/the-sale-of-san-franciscos-institutions/', date: '2023-05-21' },
   { title: 'San Francisco’s Congressional Race May Predict the Future of the Democratic Party', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2026/01/09/san-franciscos-congressional-race-may-predict-the-future-of-the-democratic-party/', date: '2026-01-09' },
 ];
