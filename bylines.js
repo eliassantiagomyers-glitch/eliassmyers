897bf1fb9ec4e6aafd4cdde473e7181a438b0a79
elias myers <eliasmyers@wifi-10-43-72-225.wifi.berkeley.edu>
2026-05-07 13:12:57 -0700

    byline: Last Chopper Out of Saigon, or a Dispatch from the Underclass (Medium)

--- bylines.js
+++ bylines.js
@@ -3,6 +3,7 @@
 // { title: "Article title", publication: "Publication Name", url: "https://...", date: "YYYY-MM-DD" }
 
 var BYLINES = [
+  { title: 'Last Chopper Out of Saigon, or a Dispatch from the Underclass', publication: 'Medium', url: 'https://medium.com/the-secret-history-of-america/last-chopper-out-of-saigon-or-a-dispatch-from-the-underclass-805dc3e17c28', date: '2026-05-06' },
   { title: 'Fiat Lux Darkens with Peyrin Kao's Firing', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2025/12/08/fiat-lux-darkens-with-peyrin-kaos-firing/', date: '2025-12-08' },
   { title: 'The Sale of San Francisco's Institutions', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2023/05/21/the-sale-of-san-franciscos-institutions/', date: '2023-05-21' },
   { title: 'San Francisco’s Congressional Race May Predict the Future of the Democratic Party', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2026/01/09/san-franciscos-congressional-race-may-predict-the-future-of-the-democratic-party/', date: '2026-01-09' },
