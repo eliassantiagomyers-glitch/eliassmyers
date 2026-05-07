37f2baffe4a6c4ace20bf612ae850bd40670ae32
elias myers <eliasmyers@wifi-10-43-72-225.wifi.berkeley.edu>
2026-05-07 13:08:48 -0700

    byline: Prescribed Burns as the Solution to California's Increasingly Incendiary Forests (Berkeley Political Review)

--- site2/bylines.js
+++ site2/bylines.js
@@ -3,5 +3,6 @@
 // { title: "Article title", publication: "Publication Name", url: "https://...", date: "YYYY-MM-DD" }
 
 var BYLINES = [
+  { title: 'Prescribed Burns as the Solution to California's Increasingly Incendiary Forests', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2022/12/04/prescribed-burns-as-the-solution-to-californias-increasingly-incendiary-forests/', date: '2022-12-04' },
   { title: 'San Francisco’s Congressional Race May Predict the Future of the Democratic Party', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2026/01/09/san-franciscos-congressional-race-may-predict-the-future-of-the-democratic-party/', date: '2026-01-09' },
 ];
