ef295d2c3a4c2371e3368c2ce0475a6c7b2f0c96
elias myers <eliasmyers@wifi-10-43-72-225.wifi.berkeley.edu>
2026-05-07 13:12:19 -0700

    byline: Fiat Lux Darkens with Peyrin Kao's Firing (Berkeley Political Review)

--- bylines.js
+++ bylines.js
@@ -3,6 +3,7 @@
 // { title: "Article title", publication: "Publication Name", url: "https://...", date: "YYYY-MM-DD" }
 
 var BYLINES = [
+  { title: 'Fiat Lux Darkens with Peyrin Kao's Firing', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2025/12/08/fiat-lux-darkens-with-peyrin-kaos-firing/', date: '2025-12-08' },
   { title: 'The Sale of San Francisco's Institutions', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2023/05/21/the-sale-of-san-franciscos-institutions/', date: '2023-05-21' },
   { title: 'San Francisco’s Congressional Race May Predict the Future of the Democratic Party', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2026/01/09/san-franciscos-congressional-race-may-predict-the-future-of-the-democratic-party/', date: '2026-01-09' },
 ];
