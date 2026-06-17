a1277205e1ae7bb6096e4096ee2fea74e225bf8e
Elias Myers <eliasssantiagomyers@berkeley.edu>
2026-06-16 17:33:31 -0700

    byline: Rave Against the Machine (Berkeley Political Review)

--- bylines.js
+++ bylines.js
@@ -3,6 +3,7 @@
 // { title: "Article title", publication: "Publication Name", url: "https://...", date: "YYYY-MM-DD" }
 
 var BYLINES = [
+  { title: 'Rave Against the Machine', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2026/06/15/rave-against-the-machine/', date: '2026-06-15' },
   { title: 'Last Chopper Out of Saigon, or a Dispatch from the Underclass', publication: 'Medium', url: 'https://medium.com/the-secret-history-of-america/last-chopper-out-of-saigon-or-a-dispatch-from-the-underclass-805dc3e17c28', date: '2026-05-06' },
   { title: 'Fiat Lux Darkens with Peyrin Kao\'s Firing', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2025/12/08/fiat-lux-darkens-with-peyrin-kaos-firing/', date: '2025-12-08' },
   { title: 'The Sale of San Francisco\'s Institutions', publication: 'Berkeley Political Review', url: 'https://bpr.studentorg.berkeley.edu/2023/05/21/the-sale-of-san-franciscos-institutions/', date: '2023-05-21' },
