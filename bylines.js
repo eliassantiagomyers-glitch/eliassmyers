becd83e3e3ba65b9d8fa848ea4407ae023ca491d
Elias Myers <eliasssantiagomyers@berkeley.edu>
2026-09-16 14:23:59 -0700

    byline: Council delays action on proposed ad hoc oversight committee (ChicoSol)

--- bylines.js
+++ bylines.js
@@ -3,6 +3,7 @@
 // { title: "Article title", publication: "Publication Name", url: "https://...", date: "YYYY-MM-DD" }
 
 var BYLINES = [
+  { title: 'Council delays action on proposed ad hoc oversight committee', publication: 'ChicoSol', url: 'https://chicosol.org/2026/09/16/council-delays-action-on-proposed-ad-hoc-oversight-committee/', date: '2026-09-16' },
   { title: 'Community members call for ALPR, police oversight', publication: 'ChicoSol', url: 'https://chicosol.org/2026/08/18/community-members-call-for-alpr-police-oversight/', date: '2026-08-19' },
   { title: 'County jail responds to disability rights complaints', publication: 'ChicoSol', url: 'https://chicosol.org/2026/08/11/county-jail-responds-to-disability-rights-complaints/', date: '2026-08-11' },
   { title: 'Police chief responds to data sharing controversy', publication: 'ChicoSol', url: 'https://chicosol.org/2026/08/05/police-chief-responds-to-data-sharing-controversy/', date: '2026-08-05' },
